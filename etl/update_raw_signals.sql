3c4f043c4b55f890f12b08b6dc5e300bd88ae2dc
Nitzan Aviram <nitzanav@gmail.com>
2026-09-21 14:56:36 +0300

    update raw signals sql

--- etl/update_raw_signals.sql
+++ etl/update_raw_signals.sql
@@ -5,6 +5,9 @@ CREATE TABLE IF NOT EXISTS yahoo_all_symbols (
   percent real,
   projected_price real
 );
+ALTER TABLE yahoo_all_symbols ADD COLUMN IF NOT EXISTS projected_price real;
+TRUNCATE TABLE yahoo_all_symbols;
+\copy yahoo_all_symbols (date, ticker, analyst, percent, projected_price) FROM 'data/analyst_recomendation/yahoo/all_symbols.csv' CSV HEADER
 
 CREATE TABLE IF NOT EXISTS google_all_symbols (
   date date,
@@ -14,11 +17,7 @@ CREATE TABLE IF NOT EXISTS google_all_symbols (
   projected_price real
 );
 
-ALTER TABLE yahoo_all_symbols ADD COLUMN IF NOT EXISTS projected_price real;
 ALTER TABLE google_all_symbols ADD COLUMN IF NOT EXISTS projected_price real;
-
-TRUNCATE TABLE yahoo_all_symbols;
 TRUNCATE TABLE google_all_symbols;
-
-\copy yahoo_all_symbols (date, ticker, analyst, percent, projected_price) FROM 'data/analyst_recomendation/yahoo/all_symbols.csv' CSV HEADER
-\copy google_all_symbols (date, ticker, analyst, percent, projected_price) FROM 'data/analyst_recomendation/google/all_symbols.csv' CSV HEADER
+-- focusing now only on yahoo
+-- \copy google_all_symbols (date, ticker, analyst, percent, projected_price) FROM 'data/analyst_recomendation/google/all_symbols.csv' CSV HEADER
